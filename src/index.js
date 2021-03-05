80d205621fdbbee381ee4e83e4777387c46c0c67
prokhorovd <github@prokhorov.dev>
2021-03-05 19:14:28 +0400

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let result = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < result) {
+      result = array[i]
+    }
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let result = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > result) {
+      result = array[i]
+    }
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  return sum/array.length;
 }
